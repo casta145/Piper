7154e4738df39b347f9a82802a3570a447d74bdb
casta145 <casta145@mail.chapman.edu>
2018-11-02 19:19:49 -0700

    Created 1st screen & the next 2 intent activites

--- app/src/main/java/com/example/alfonsoc/piper/PiperLogin.java
+++ app/src/main/java/com/example/alfonsoc/piper/PiperLogin.java
@@ -1,7 +1,10 @@
 package com.example.alfonsoc.piper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class PiperLogin extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class PiperLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        Button createAcc = findViewById(R.id.cretAcc);
+        Button loginAcc = findViewById(R.id.lgin);
+
+        createAcc.setOnClickListener(acctCreation);
+        loginAcc.setOnClickListener(acctLogin);
     }
+
+    private View.OnClickListener acctCreation = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent create = new Intent(PiperLogin.this, CreateAcc.class);
+            startActivity(create);
+        }
+    };
+
+    private View.OnClickListener acctLogin = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent login = new Intent(PiperLogin.this, AccLogin.class);
+            startActivity(login);
+        }
+    };
 }
