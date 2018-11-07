bd01999e312eaf87f16fd9ae5376294d47f68137
casta145 <casta145@mail.chapman.edu>
2018-11-07 15:39:56 -0800

    Worked on create,acc classes and their layouts. As well as created AccFix and its layout.

---
+++ app/src/main/java/com/example/alfonsoc/piper/AccFix.java
@@ -0,0 +1,13 @@
+package com.example.alfonsoc.piper;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AccFix extends AppCompatActivity {
+    //asl user for email associated with account and DOB
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acc_fix_layout);
+    }
+}
--- app/src/main/java/com/example/alfonsoc/piper/AccLogin.java
+++ app/src/main/java/com/example/alfonsoc/piper/AccLogin.java
@@ -1,7 +1,12 @@
 package com.example.alfonsoc.piper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AccLogin extends AppCompatActivity{
 
@@ -9,5 +14,29 @@ public class AccLogin extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_layout);
+
+        Button login = findViewById(R.id.loginbutton);
+
+        EditText username = findViewById(R.id.userinput);
+        EditText password = findViewById(R.id.passinput);
+        TextView userpass = findViewById(R.id.clicklink);
+
+        login.setOnClickListener(logintoprofile);
+        userpass.setOnClickListener(fixuserpass);
     }
+
+    private View.OnClickListener logintoprofile = new View.OnClickListener() {
+        @Override
+        public void onClick (View view){
+
+        }
+    };
+
+    private View.OnClickListener fixuserpass = new View.OnClickListener() {
+        @Override
+        public void onClick (View view){
+            Intent test = new Intent(AccLogin.this, AccFix.class);
+            startActivity(test);
+        }
+    };
 }
