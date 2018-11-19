3d204da6774262eec0e893ff40b2cc04818c9f3e
tan177 <tan177@mail.chapman.edu>
2018-11-18 22:13:21 -0800

    still working on Homepage bottom navigation

---
+++ app/src/main/java/com/example/alfonsoc/piper/UploadPhoto.java
@@ -0,0 +1,13 @@
+package com.example.alfonsoc.piper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadPhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_photo);
+    }
+}
